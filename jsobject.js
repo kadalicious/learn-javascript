02f3abf084b33cb377c9949e170afabb53784d96
Herdo Dimas Pratirto <119818225+kadalicious@users.noreply.github.com>
2023-09-01 20:01:13 +0700

    coba object

---
+++ jsobject.js
@@ -0,0 +1,17 @@
+const identitas = {nama:"herdo", umur:"20 tahun", agama:"islam"};
+ 
+console.log("nama saya " + identitas.nama + ", umur saya " + identitas.umur + ", dan agama saya " + identitas.agama);
+
+
+
+
+function nama(){
+    return identitas.agama;
+}
+
+console.log(nama());
+
+var nama = identitas.nama;
+console.log(nama);
+
+
