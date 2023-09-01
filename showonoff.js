bb22d09ad7912d9fb4cfbcee468178edacfce464
Herdo Dimas Pratirto <119818225+kadalicious@users.noreply.github.com>
2023-09-01 11:17:53 +0700

    show off and on text

---
+++ showonoff.js
@@ -0,0 +1,20 @@
+var showon = document.getElementById("showon");
+showon.addEventListener{
+    showonok();
+}
+
+function showonok(){
+    showon.style.display='block';
+}
+
+var showoff = document.getElementById("showoff");
+showoff.addEventListener{
+    showoffok();
+}
+function showoffok(){
+    showoff.style.display='none';
+}
+
+
+var text = document.getElementById("text");
+
